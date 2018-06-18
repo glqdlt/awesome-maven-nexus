2f068100d1156ec29788bb3f3a77b171848b681c
glqdlt <dlfdnd0725@gmail.com>
2018-06-18 18:06:38 +0900

    project init

---
+++ src/main/java/com/fourones/webteam/example/awesomemavennexus/AwesomeMavenNexusApplication.java
@@ -0,0 +1,12 @@
+package com.fourones.webteam.example.awesomemavennexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeMavenNexusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwesomeMavenNexusApplication.class, args);
+    }
+}
